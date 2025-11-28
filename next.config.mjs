0328867818dcc8d71ee24d7cd1749cdae8e635e3
fazetdev <fazetdev@gmail.com>
2025-11-28 20:03:33 +0100

    Add proper i18n configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-import withNextIntl from 'next-intl/plugin');
- 
+import withNextIntl from 'next-intl/plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  // i18n config for next-intl
+  intl: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+};
+
 export default withNextIntl(nextConfig);
